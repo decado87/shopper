c04f9e01faa8fcdb00ac23d96c4992b05481cc0f
Tomas Janusek <tomas.janusek@goodrequest.com>
2019-08-08 14:38:29 +0200

    beta version

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'categories#index'
+
+  resources :categories, only: [:index] do
+    resources :products, only: [:index]
+  end
+
+  get '/cart', to: 'order_items#index'
+  resources :order_items, path: '/cart/items'
 end

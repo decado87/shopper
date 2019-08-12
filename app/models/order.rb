06e2918399e591f5a6b3b7fc7aa319ce5352856b
decado87 <janusek.t@gmail.com>
2019-08-12 22:05:04 +0200

    fixed minus function in cart

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,8 +2,4 @@ class Order < ApplicationRecord
 
   has_many :items, class_name: 'OrderItem', dependent: :delete_all
 
-  # def find_order(id:)
-  #   @order = Order.find(id: id, status: 'cart')
-  # end
-
 end
